09b50d7d5b7ef1d41c4398c486cdf235f21cc673
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-13 01:07:07 +0700

    feat: responsive font size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="h-screen flex flex-col gap-4 items-center justify-center">
   <h1 class="text-5xl font-semibold">Hi! :D</h1>
-  <h3 class="text-center font-light">My name is Hadziq,<br>
+  <h3 class="text-xs sm:text-base text-center font-light">My name is Hadziq,<br>
     and you'll see my porto here,, soon.. I hope 😅<br>
     ((when I'm not being lazy))
   </h3>
