a5c1ca41f467f1119bdd7c05373ae482409de4c8
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-16 23:10:35 +0700

    feat: social accounts

--- src/lib/First.svelte
+++ src/lib/First.svelte
@@ -1,4 +1,13 @@
-<div class="flex flex-col gap-4 items-center">
+<script lang="ts">
+  import IconSocial from "./components/IconSocial.svelte"
+
+  import BsGithub from "svelte-icons-pack/bs/BsGithub"
+  import BsLinkedin from "svelte-icons-pack/bs/BsLinkedin"
+  import BsInstagram from "svelte-icons-pack/bs/BsInstagram"
+  import SiGmail from "svelte-icons-pack/si/SiGmail"
+</script>
+
+<div class="flex flex-col gap-6 items-center">
   <h1 class="absolute -translate-y-[120%] text-5xl sm:text-[5rem] text-dark font-extrabold">
     Hi! <span class="text-primary">:D</span>
   </h1>
@@ -9,5 +18,27 @@
     <p class="text-[0.65rem] sm:text-sm text-center text-dark/50 font-light">
       Other names: Hajik, Hajiq, Haziq, Hazik, Ojin, wait what.
     </p>
+  </div>
+  <div class="w-full flex gap-1 justify-around">
+    <IconSocial
+      social={BsGithub}
+      title={"Github"}
+      link={"https://github.com/mhadziqrazin"}
+    />
+    <IconSocial
+      social={BsLinkedin}
+      title={"Linkedin"}
+      link={"https://www.linkedin.com/in/mhadziqrazin/"}
+    />
+    <IconSocial
+      social={BsInstagram}
+      title={"Instagram"}
+      link={"https://www.instagram.com/hadziqrazin/"}
+    />
+    <IconSocial
+      social={SiGmail}
+      title={"Instagram"}
+      link={"mailto:mhadziqrazin@gmail.com"}
+    />
   </div>
 </div>
---
+++ src/lib/components/IconSocial.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Icon from "svelte-icons-pack";
+    export let social: Icon
+    export let title: string
+    export let link: string
+</script>
+
+<div class="opacity-30 hover:cursor-pointer hover:opacity-75 transition-all duration-200">
+  <a href={link} target="_blank">
+    <Icon src={social} size=25 title={title} />
+  </a>
+</div>
