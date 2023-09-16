c3524c995e2b806507e3e2bd1e6492116b3df374
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-16 22:24:17 +0700

    style: text opacity

--- src/lib/First.svelte
+++ src/lib/First.svelte
@@ -3,10 +3,10 @@
     Hi! <span class="text-primary">:D</span>
   </h1>
   <div class="flex flex-col gap-1">
-    <h3 class="text-lg sm:text-2xl text-center text-dark font-normal">
+    <h3 class="text-lg sm:text-2xl text-center text-dark/80 font-normal">
       <span class="font-light">I am</span> Muhammad <span class="font-bold text-secondary">Hadziq</span> Razin
     </h3>
-    <p class="text-[0.65rem] sm:text-sm text-center text-dark font-light">
+    <p class="text-[0.65rem] sm:text-sm text-center text-dark/50 font-light">
       Other names: Hajik, Hajiq, Haziq, Hazik, Ojin, wait what.
     </p>
   </div>
