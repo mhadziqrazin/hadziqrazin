d2c3facd652f442f31cf165ec799f95063709366
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-16 23:17:46 +0700

    refactor: layout padding and bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-  import First from "$lib/First.svelte";
+  import First from '$lib/First.svelte'
 </script>
 
-<div class="px-4 bg-light">
-  <div class="screen-height flex items-center justify-center transition-all duration-300 ease-out">
-    <First />
-  </div>
+<div class="screen-height flex items-center justify-center transition-all duration-300 ease-out">
+  <First />
 </div>
