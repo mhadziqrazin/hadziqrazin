376fbc6f173bc50c44fda450a8fe911e1b292066
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-17 00:01:26 +0700

    fix: typof error

--- src/lib/components/IconSocial.svelte
+++ src/lib/components/IconSocial.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import Icon from "svelte-icons-pack";
-    export let social: Icon
-    export let title: string
-    export let link: string
+  import Icon from 'svelte-icons-pack/Icon.svelte'
+
+  export let social: any
+  export let title: string
+  export let link: string
 </script>
 
 <div class="opacity-30 hover:cursor-pointer hover:opacity-75 transition-all duration-200">
   <a href={link} target="_blank">
-    <Icon src={social} size=25 title={title} />
+    <Icon src={social} size="25" {title} />
   </a>
 </div>
