3ee4667870f84500dc631c952f465b7e45c22c26
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-17 23:03:21 +0700

    feat: all rights reserved word

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,10 @@
 import First from '$lib/sections/First.svelte'
 </script>
 
-<div class="flex flex-col items-center">
+<div class="flex flex-col items-center pb-8">
   <First />
   <About />
+  <p class="font-light text-dark/70">
+    Created by <span class="font-semibold">Muhammad Hadziq Razin</span> © 2023. All Rights Reserved.
+  </p>
 </div>
