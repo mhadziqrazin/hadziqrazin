0783bf57fef8b8b895b312d89d80d160f9c37fa8
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-17 23:05:26 +0700

    style: change screen hegiht location

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
   import First from '$lib/sections/First.svelte'
 </script>
 
-<div class="flex flex-col items-center pb-8">
+<div class="screen-height flex flex-col justify-center items-center pb-8">
   <First />
-  <p class="font-light text-dark/70">
+  <p class="absolute bottom-8 font-light text-dark/70">
     Created by <span class="font-semibold">Muhammad Hadziq Razin</span> © 2023. All Rights Reserved.
   </p>
 </div>
