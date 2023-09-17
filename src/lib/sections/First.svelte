b55ae578bbc9f531c4904a02b24fa6b159c7721f
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-18 01:06:16 +0700

    feat: about me in first section

--- src/lib/sections/First.svelte
+++ src/lib/sections/First.svelte
@@ -7,7 +7,7 @@
   import SiGmail from "svelte-icons-pack/si/SiGmail"
 
   import { onMount } from "svelte"
-  import { fly } from "svelte/transition"
+  import { fade, fly } from "svelte/transition"
 
   let animate = false
   onMount(() => {
@@ -15,50 +15,69 @@
   })
 </script>
 
-<section class="flex flex-col gap-6 items-center justify-center">
+<section class="flex flex-col md:w-1/2 gap-2 md:gap-6 items-center justify-center">
   {#if animate}
-    <h1 in:fly={{y:-10, duration: 1000}} class="text-5xl sm:text-[5rem] text-dark font-extrabold">
+    <h1 in:fly={{y:-10, duration: 1000}} class="text-4xl md:text-[4rem] text-dark font-extrabold">
       Hi! <span class="text-primary">:D</span>
     </h1>
 
-    <div class="flex flex-col gap-1">
-      <h3 in:fly={{y:-10, duration: 1000, delay: 200}} class="text-lg sm:text-2xl text-center text-dark/80 font-normal">
-        <span class="font-light">I&apos;m</span> Muhammad <span class="font-bold text-secondary">Hadziq</span> Razin
-      </h3>
+    <h3 in:fly={{y:-10, duration: 1000, delay: 200}} class="text-4xl text-dark/90 font-semibold">
+    </h3>
+    
+    
+    <p in:fade={{duration: 1000, delay: 200}} class="text-lg md:text-3xl font-semibold text-dark/70">
+      <span class="text-dark/90">Muhammad <span class="font-bold text-secondary">Hadziq</span> Razin</span>,
       
-      <p in:fly={{y:-10, duration: 1000, delay: 400}} class="text-[0.65rem] sm:text-sm text-center text-dark/50 font-light">
+      an undergraduate
+      
+      <a href="https://cs.ui.ac.id" target="_blank" class="underline decoration-[3px]">
+        computer science
+      </a>
+      
+      student at
+      
+        <a href="https://ui.ac.id" target="_blank" class="underline decoration-[3px]">
+        University of Indonesia,
+      </a>
+
+      who&apos;s passionate in
+      <span class="text-dark/90">software engineering</span>.
+    </p>
+    
+    <div class="flex flex-col gap-6">
+      <p in:fly={{y:-5, duration: 1000, delay: 400}} class="text-[0.65rem] text-dark/50 font-light">
         Other names: Hajik, Hajiq, Haziq, Hazik, Ojin, wait what.
       </p>
-    </div>
-
-    <div class="w-full flex gap-1 justify-around py-2">
-      <div in:fly={{x:-10, duration: 1000, delay: 400}}>
-        <IconSocial
-          social={BsGithub}
-          title={"Github"}
-          link={"https://github.com/mhadziqrazin"}
-        />
-      </div>
-      <div in:fly={{x:-10, duration: 1000, delay: 550}}>
-        <IconSocial
-          social={BsLinkedin}
-          title={"Linkedin"}
-          link={"https://www.linkedin.com/in/mhadziqrazin/"}
-        />
-      </div>
-      <div in:fly={{x:-10, duration: 1000, delay: 700}}>
-        <IconSocial
-          social={BsInstagram}
-          title={"Instagram"}
-          link={"https://www.instagram.com/hadziqrazin/"}
-        />
-      </div>
-      <div in:fly={{x:-10, duration: 1000, delay: 950}}>
-        <IconSocial
-          social={SiGmail}
-          title={"Mail"}
-          link={"mailto:mhadziqrazin@gmail.com"}
-        />
+      
+      <div class="w-full flex gap-1 justify-around py-2">
+        <div in:fly={{x:-10, duration: 1000, delay: 400}}>
+          <IconSocial
+            social={BsGithub}
+            title={"Github"}
+            link={"https://github.com/mhadziqrazin"}
+          />
+        </div>
+        <div in:fly={{x:-10, duration: 1000, delay: 550}}>
+          <IconSocial
+            social={BsLinkedin}
+            title={"Linkedin"}
+            link={"https://www.linkedin.com/in/mhadziqrazin/"}
+          />
+        </div>
+        <div in:fly={{x:-10, duration: 1000, delay: 700}}>
+          <IconSocial
+            social={BsInstagram}
+            title={"Instagram"}
+            link={"https://www.instagram.com/hadziqrazin/"}
+          />
+        </div>
+        <div in:fly={{x:-10, duration: 1000, delay: 950}}>
+          <IconSocial
+            social={SiGmail}
+            title={"Mail"}
+            link={"mailto:mhadziqrazin@gmail.com"}
+          />
+        </div>
       </div>
     </div>
   {/if}
