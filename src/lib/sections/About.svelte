f995e1ac284023999e9f41468559674689e7e8f1
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-17 23:03:45 +0700

    refactor: delete about section

--- src/lib/sections/About.svelte
+++
@@ -1,5 +0,0 @@
-<section class="screen-height flex flex-col justify-center">
-  <h1 class="text-5xl font-semibold">
-    About Me
-  </h1>
-</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
-  import About from '$lib/sections/About.svelte'
-import First from '$lib/sections/First.svelte'
+  import First from '$lib/sections/First.svelte'
 </script>
 
 <div class="flex flex-col items-center pb-8">
   <First />
-  <About />
   <p class="font-light text-dark/70">
     Created by <span class="font-semibold">Muhammad Hadziq Razin</span> © 2023. All Rights Reserved.
   </p>
