cfabd063309373c2aa231b56f1b2d4083306abab
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-19 06:57:57 +0700

    style: padding on top decreased

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import First from '$lib/sections/First.svelte'
 </script>
 
-<div class="screen-height flex flex-col justify-center items-center py-8">
+<div class="screen-height flex flex-col justify-center items-center pb-8">
   <First />
   <p class="absolute bottom-8 text-[0.5rem] lg:text-base font-light text-dark/70">
     Created by <span class="font-semibold">Muhammad Hadziq Razin</span> &copy; 2023. All Rights Reserved.
