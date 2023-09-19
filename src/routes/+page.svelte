419108ce8f5828c88e160ca2781315f3a5f04253
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-19 07:35:49 +0700

    feat: copyright to div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
 
 <div class="screen-height flex flex-col justify-center items-center pb-8">
   <First />
-  <p class="absolute bottom-8 text-[0.5rem] lg:text-base font-light text-dark/80">
-    Created by <span class="font-semibold">Muhammad Hadziq Razin</span> &copy; 2023. All Rights Reserved.
-  </p>
+  
+  <div class="absolute bottom-0 pb-8">
+    <p class="text-[0.5rem] lg:text-base font-light text-dark/80">
+      Created by <span class="font-semibold">Muhammad Hadziq Razin</span> &copy; 2023. All Rights Reserved.
+    </p>
+  </div>
 </div>
