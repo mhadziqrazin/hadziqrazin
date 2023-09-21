c451e772a2b4feef93ec48aba206d14d580b4aa7
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-21 08:52:22 +0700

    feat: typescript constant

---
+++ src/lib/constants/techs/typeScript.ts
@@ -0,0 +1,11 @@
+import type { Tech } from '$lib/types/project'
+import logo from '$lib/assets/techs/typescript-official-svgrepo-com.svg'
+
+const typeScript: Tech = {
+  name: 'TypeScript',
+  color: '#3178c6',
+  img: logo,
+  url: 'https://www.typescriptlang.org/'
+}
+
+export default typeScript
