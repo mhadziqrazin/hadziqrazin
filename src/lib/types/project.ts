c3709156ecb3c29a7d5f9e1e3f7a4b450f3ddbed
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-21 08:26:43 +0700

    feat: type tech

--- src/lib/types/project.ts
+++ src/lib/types/project.ts
@@ -2,4 +2,12 @@ export interface Project {
   img: string
   name: string
   description: string
+  techs: Tech[]
+}
+
+export interface Tech {
+  name: string
+  color: string
+  logo: string
+  url: string
 }
