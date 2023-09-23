03a4f3169f11f777c63e0478c7f5a53ca5869361
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-23 17:20:27 +0700

    refactor: cleaning code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
-  import IconSocial from "$lib/components/IconSocial.svelte"
-
-  import BsGithub from "svelte-icons-pack/bs/BsGithub"
-  import BsLinkedin from "svelte-icons-pack/bs/BsLinkedin"
-  import BsInstagram from "svelte-icons-pack/bs/BsInstagram"
-  import SiGmail from "svelte-icons-pack/si/SiGmail"
-
   import { onMount } from "svelte"
-  import { fade, fly } from "svelte/transition"
-    import Socials from "$lib/components/Socials.svelte"
-    import About from "$lib/components/sections/About.svelte"
+  import About from "$lib/components/sections/About.svelte"
 
   let animate = false
   onMount(() => {
@@ -19,10 +10,10 @@
 
 <svelte:head>
   <title>Hadziq Razin</title>
-  <meta property="og:title" content="Muhammad Hadziq Razin's Portfolio | Home Page">
-  <meta property="og:description" content="Muhammad Hadziq Razin's Portfolio | Home Page">
-  <meta name="description" content="Muhammad Hadziq Razin's Portfolio | Home Page">
-  <meta name="keywords" content="muhammad hadziq razin, muhammad, hadziq, razin, hajik, ojin">
+  <meta property="og:title" content="Muhammad Hadziq Razin's Portfolio | Home Page" />
+  <meta property="og:description" content="Muhammad Hadziq Razin's Portfolio | Home Page" />
+  <meta name="description" content="Muhammad Hadziq Razin's Portfolio | Home Page" />
+  <meta name="keywords" content="muhammad hadziq razin, muhammad, hadziq, razin, hajik, ojin" />
 </svelte:head>
 
 {#key animate}
