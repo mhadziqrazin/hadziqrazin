44b6d8dc1d596670a71e4f07ce27bf082023a4e7
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-25 16:33:41 +0700

    feat: scroll down button

--- src/lib/components/sections/About.svelte
+++ src/lib/components/sections/About.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   import { fly, fade } from "svelte/transition"
   import Socials from "../Socials.svelte"
+  import Icon from "svelte-icons-pack"
+  import AiOutlineArrowDown from "svelte-icons-pack/ai/AiOutlineArrowDown"
+
+  let section: HTMLElement
+
+  function scrollDown() {
+    window.scrollTo({top: section.offsetHeight, behavior: "smooth"})
+  }
 </script>
 
 
-<section class="container mx-auto px-4 min-h-screen md:w-1/2 flex flex-col justify-center items-center gap-4 lg:gap-10">
+<section bind:this={section} class="container relative mx-auto px-4 min-h-screen md:w-1/2 flex flex-col justify-center items-center gap-4 lg:gap-10">
   <h1 in:fly={{y:20, duration: 1000}} class="text-4xl lg:text-[4rem] text-dark font-extrabold">
     Hi! <span class="text-primary">:D</span>
   </h1>
@@ -41,4 +49,8 @@
   
   <!-- SOCIAL LINKS -->
   <Socials />
+
+  <button on:click={scrollDown} class="animate-bounce absolute bottom-20 opacity-70">
+    <Icon src={AiOutlineArrowDown} size={20} />
+  </button>
 </section>
