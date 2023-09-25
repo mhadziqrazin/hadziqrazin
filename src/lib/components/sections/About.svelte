5d02d78edeb40e5de41d3919d033b059be94a738
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-25 18:02:21 +0700

    feat: profile picture

--- src/lib/components/sections/About.svelte
+++ src/lib/components/sections/About.svelte
@@ -4,6 +4,9 @@
   import Icon from "svelte-icons-pack"
   import AiOutlineArrowDown from "svelte-icons-pack/ai/AiOutlineArrowDown"
 
+  import image from "$lib/assets/Hadziq Razin.jpg"
+    import { flip } from "svelte/animate"
+
   let section: HTMLElement
   let y: number
   
@@ -12,14 +15,32 @@
   function scrollDown() {
     window.scrollTo({top: section.offsetHeight, behavior: "smooth"})
   }
+
+  let profile = false
+  function toggle() {
+    profile = !profile
+  }
 </script>
 
 <svelte:window bind:scrollY={y} />
 
+{#if profile}
+<section class="min-h-screen flex flex-col justify-center">
+  <button in:fly={{y: 20, duration: 500}} on:click={toggle} class="rounded-full border-dark border-4">
+    <img
+    src={image}
+    alt="Muhammad Hadziq Razin"
+    class="max-w-[60vw] md:max-w-[30vw] aspect-square rounded-full border-primary border-4"
+    />
+  </button>
+</section>
+
+{:else}
+
 {#key animate}
   <section bind:this={section} class={`${animate ? 'opacity-100' : 'opacity-0'} container relative mx-auto px-4 min-h-screen md:w-1/2 flex flex-col justify-center items-center gap-4 lg:gap-10`}>
-    <h1 in:fly={{y:20, duration: 1000}} class="text-4xl lg:text-[4rem] text-dark font-extrabold">
-      Hi! <span class="text-primary">:D</span>
+    <h1 in:fly={{y:20, duration: 1000}} class="text-4xl lg:text-[4rem] lg:leading-[4.5rem] text-dark font-extrabold">
+      Hi! <button on:click={toggle} class="text-primary animate-pulse hover:-translate-y-1 transition-all duration-300">:D</button>
     </h1>
     
     <div class="flex flex-col gap-1 lg:gap-3">
@@ -60,3 +81,4 @@
     </button>
   </section>
 {/key}
+{/if}
--- src/lib/components/sections/Showcase.svelte
+++ src/lib/components/sections/Showcase.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+  const titles = ["Projects"]
+  let selected = 0
+</script>
+
+
 <section class="min-h-screen flex flex-col items-center justify-center">
   <h1>
-    Showcase
+    {titles[selected]}
   </h1>
 </section>
