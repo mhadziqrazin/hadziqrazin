ade94f51cd58d2b23a06939966766c9887bbbc0f
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-23 22:28:38 +0700

    refactor: change layout to main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
   import '../app.css'
 </script>
 
-<div class="mx-auto overflow-x-clip">
-  <slot />
+<div class="mx-auto overflow-x-clip min-h-screen flex flex-col justify-center">
+  <div class="flex flex-grow justify-center">
+    <slot />
+  </div>
+  <div class="py-8 w-full flex justify-center">
+    <p class="text-[0.5rem] lg:text-base font-light text-dark/80">
+      Created by <span class="font-semibold">Muhammad Hadziq Razin</span> &copy; 2023. All Rights Reserved.
+    </p>
+  </div>
 </div>
