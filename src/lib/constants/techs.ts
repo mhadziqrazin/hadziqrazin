f0dae2244d099cbf8f7be7dd8fbe252a6ec67a51
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 11:29:29 +0700

    feat: tech svelte

--- src/lib/constants/techs.ts
+++ src/lib/constants/techs.ts
@@ -1,9 +1,17 @@
 import type { Tech } from '$lib/types/project'
-import logo from '$lib/assets/techs/typescript-official-svgrepo-com.svg'
+import typeScriptLogo from '$lib/assets/techs/Logo-official-svgrepo-com.svg'
+import svelteLogo from '$lib/assets/techs/svelte-icon-svgrepo-com.svg'
 
 export const typeScript: Tech = {
   name: 'TypeScript',
   color: '#3178c6',
-  img: logo,
+  img: typeScriptLogo,
   url: 'https://www.typescriptlang.org/'
+}
+
+export const svelte: Tech = {
+  name: 'Svelte',
+  color: '#FF3E00',
+  img: svelteLogo,
+  url: 'https://svelte.dev/'
 }
