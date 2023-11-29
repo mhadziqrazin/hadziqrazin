93f5490bce34d7199cd94423a4912aa6ad65a931
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 10:32:23 +0700

    refactor: constants folder structure

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -1,13 +1,11 @@
 import type { Project } from '$lib/types/project'
 import image from '$lib/assets/projects/todo.png'
-import typeScript from '../techs/typeScript'
+import { typeScript } from './techs'
 
-const toDo: Project = {
+export const toDo: Project = {
   img: image,
   name: 'To Do Hajik',
   description: 'To do list with feeds feature where you can share your completed task',
   techs: [typeScript],
   url: 'https://to.do.hadziqrazin.com'
 }
-
-export default toDo
