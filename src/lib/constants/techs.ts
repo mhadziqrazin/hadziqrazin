6f2615cf3e7f79c1c3619c13ef7987e708aa5b81
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 15:57:30 +0700

    refactor: change image tech to icon

--- src/lib/constants/techs.ts
+++ src/lib/constants/techs.ts
@@ -1,33 +1,33 @@
 import type { Tech } from '$lib/types/project'
-import typeScriptLogo from '$lib/assets/techs/Logo-official-svgrepo-com.svg'
-import svelteLogo from '$lib/assets/techs/svelte-icon-svgrepo-com.svg'
-import nextjsLogo from '$lib/assets/techs/nextjs-icon-svgrepo-com.svg'
-import tailwindLogo from '$lib/assets/techs/tailwind-svgrepo-com.svg'
+import SiTypescript from 'svelte-icons-pack/si/SiTypescript'
+import SiSvelte from 'svelte-icons-pack/si/SiSvelte'
+import SiNextdotjs from 'svelte-icons-pack/si/SiNextdotjs'
+import SiTailwindcss from 'svelte-icons-pack/si/SiTailwindcss'
 
 export const typeScript: Tech = {
   name: 'TypeScript',
   color: '#3178c6',
-  img: typeScriptLogo,
+  img: SiTypescript,
   url: 'https://www.typescriptlang.org/'
 }
 
 export const svelte: Tech = {
   name: 'Svelte',
   color: '#FF3E00',
-  img: svelteLogo,
+  img: SiSvelte,
   url: 'https://svelte.dev/'
 }
 
 export const nextjs: Tech = {
   name: 'Next.js',
   color: '#000',
-  img: nextjsLogo,
+  img: SiNextdotjs,
   url: 'https://nextjs.org/'
 }
 
 export const tailwind: Tech = {
   name: 'TailwindCSS',
   color: '#44a8b3',
-  img: tailwindLogo,
+  img: SiTailwindcss,
   url: 'https://tailwindcss.com/'
 }
