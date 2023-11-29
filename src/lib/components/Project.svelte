effab34bb8a4010896f815ba5dd65c111ed7667a
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 22:37:34 +0700

    feat: projects page

---
+++ src/lib/components/Project.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { Project } from "$lib/types/project"
+  import Icon from 'svelte-icons-pack'
+
+  export let project: Project
+  const techs = project.techs
+</script>
+
+<div class="relative flex flex-col min-h-full w-[300px] overflow-hidden shadow-lg hover:shadow-xl duration-200 rounded-xl bg-white/70 border-[1px] border-dark/10">
+  <!-- IMAGE -->
+  <img src={project.img} alt={project.name} class="aspect-[15/9] object-cover" >
+
+  <!-- BODY -->
+  <div class="min-h-full p-4 flex flex-col gap-2">
+    <a href={project.url} target="_blank" class="font-semibold text-xl text-dark underline w-fit">
+      {project.name}
+    </a>
+    <p class="text-dark/70 mb-[60px]">
+      {project.description}
+    </p>
+
+    <!-- TECHS -->
+    <div class="absolute bottom-4 flex gap-2">
+      {#each techs as tech}
+      <a href={tech.url} target="_blank">
+        <div class="flex gap-1 p-[9px] rounded-full hover:shadow-md duration-200" style={`background: ${tech.color}`}>
+          <Icon src={tech.img} color={'#fff'} title={tech.name} size={13} />
+        </div>
+      </a>
+      {/each}
+    </div>
+  </div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import Project from "$lib/components/Project.svelte"
   import { onMount } from "svelte"
   import { fade, fly } from "svelte/transition"
+  import { projects } from "$lib/constants/projects";
 
   let animate = false
   onMount(() => {
@@ -16,4 +18,13 @@
   <h1 in:fly={{y:20, duration: 1000}} class="text-[4rem] font-semibold text-dark">
     Projects
   </h1>
+
+  <div class="my-14 flex flex-wrap justify-center gap-10">
+    {#each projects as project}
+    <div transition:fly={{ x: 500, duration: 2000, delay: 1000 }} >
+      <Project {project} />
+    </div>
+    {/each}
+  </div>
+
 </section>
