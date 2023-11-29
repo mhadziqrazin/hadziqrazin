0b9d0abe9d8538277ca96b5278da9cf7b918a28c
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 12:22:13 +0700

    feat: tech nextjs

--- src/lib/constants/techs.ts
+++ src/lib/constants/techs.ts
@@ -1,6 +1,7 @@
 import type { Tech } from '$lib/types/project'
 import typeScriptLogo from '$lib/assets/techs/Logo-official-svgrepo-com.svg'
 import svelteLogo from '$lib/assets/techs/svelte-icon-svgrepo-com.svg'
+import nextjsLogo from '$lib/assets/techs/nextjs-icon-svgrepo-com.svg'
 
 export const typeScript: Tech = {
   name: 'TypeScript',
@@ -14,4 +15,11 @@ export const svelte: Tech = {
   color: '#FF3E00',
   img: svelteLogo,
   url: 'https://svelte.dev/'
+}
+
+export const nextjs: Tech = {
+  name: 'Next.js',
+  color: '#000',
+  img: nextjsLogo,
+  url: 'https://nextjs.org/'
 }
