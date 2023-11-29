2e29d7e6446953d13cbac574b50ae071653adfb0
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-29 22:24:27 +0700

    feat: appbilitiease and personal website

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -1,6 +1,8 @@
 import type { Project } from '$lib/types/project'
 import todoImage from '$lib/assets/projects/todo.png'
-import { tailwind, typeScript } from './techs'
+import appbilitieaseImage from '$lib/assets/projects/appbilitiease.png'
+import personalImage from '$lib/assets/projects/personal.png'
+import { nextjs, svelte, tailwind, typeScript } from './techs'
 
 export const toDo: Project = {
   img: todoImage,
@@ -9,3 +11,23 @@ export const toDo: Project = {
   techs: [typeScript, tailwind],
   url: 'https://to.do.hadziqrazin.com'
 }
+
+export const appbilitiease: Project = {
+  img: appbilitieaseImage,
+  name: 'Appbilitiease',
+  description: 'Disability support website in order to participate in Gemastik XVI 2023',
+  techs: [nextjs, typeScript, tailwind],
+  url: 'https://appbilitiease.vercel.app'
+}
+
+export const personal: Project = {
+  img: personalImage,
+  name: 'Personal Website',
+  description: "Yup, the one that you're looking at right now",
+  techs: [svelte, typeScript, tailwind],
+  url: 'https://www.hadziqrazin.com'
+}
+
+export const projects: Project[] = [
+  toDo, appbilitiease, personal
+]
