2525fce218e0ae1421458d87aa45b3d92fd4bdcd
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-30 22:56:47 +0700

    style: change life page subtitle

--- src/routes/life/+page.svelte
+++ src/routes/life/+page.svelte
@@ -3,4 +3,4 @@
   import ShowcasePage from "$lib/components/sections/ShowcasePage.svelte"
 </script>
 
-<ShowcasePage data={life} title={'Life'} subtitle={'Non-IT related works'}/>
+<ShowcasePage data={life} title={'Life'} subtitle={`Non-IT related but still makes me proud :D`}/>
