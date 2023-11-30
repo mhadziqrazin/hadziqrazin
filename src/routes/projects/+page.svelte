cc9f07bb909aa83d05b45f80e85adb1d32a5fe7a
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-30 10:31:31 +0700

    style: projects page transition

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -15,16 +15,16 @@
 </svelte:head>
 
 <section in:fade={{duration: 300}} class="flex flex-col items-center">
-  <h1 in:fly={{y:20, duration: 1000}} class="text-[4rem] font-semibold text-dark">
+  <h1 in:fly={{ y: -20, duration: 1000 }} class="text-[4rem] font-semibold text-dark">
     Projects
   </h1>
-  <h3 class="text-sm text-dark/60">
+  <h3 in:fly={{ y: -20, duration: 1000, delay: 300 }} class="text-sm text-dark/60">
     Some projects that built for learning purposes
   </h3>
 
   <div class="my-14 flex flex-wrap justify-center gap-10">
     {#each projects as project}
-    <div>
+    <div in:fade={{ duration: 300 }}>
       <Project {project} />
     </div>
     {/each}
