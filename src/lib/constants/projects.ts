3cf90ccf8346c0394bcc342e688ecbba38cb7f15
mhadziqrazin <mhadziq0403@gmail.com>
2023-11-30 10:46:53 +0700

    feat: add prisma and postgresql

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -2,13 +2,13 @@ import type { Project } from '$lib/types/project'
 import todoImage from '$lib/assets/projects/todo.png'
 import appbilitieaseImage from '$lib/assets/projects/appbilitiease.png'
 import personalImage from '$lib/assets/projects/personal.png'
-import { nextjs, svelte, tailwind, typeScript } from './techs'
+import { nextjs, postgreSql, prisma, svelte, tailwind, typeScript } from './techs'
 
 export const toDo: Project = {
   img: todoImage,
   name: 'To Do Hajik',
   description: 'To do list with feeds feature where you can share your completed task',
-  techs: [nextjs, typeScript, tailwind],
+  techs: [nextjs, typeScript, tailwind, prisma, postgreSql],
   url: 'https://to.do.hadziqrazin.com'
 }
 
