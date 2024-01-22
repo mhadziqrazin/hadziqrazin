f69c21ae2446e136bcc002926fc4ba4b60b25544
mhadziqrazin <mhadziq0403@gmail.com>
2024-01-22 15:10:44 +0700

    refactor: change pemburu movie link to youtube

--- src/lib/constants/life.ts
+++ src/lib/constants/life.ts
@@ -8,7 +8,7 @@ export const pemburu: Project = {
   name: 'Pemburu',
   description: 'Played as Polisi, a bad cop in Pemburu, a film by Sinematografi UI',
   techs: [],
-  url: 'https://drive.google.com/file/d/1pZPlBGVquqQ9EBMfY5Z4YgQdmXQWgx5a/view?usp=sharing'
+  url: 'https://youtu.be/lKsgIKfpMbA'
 }
 
 export const css: Project = {
