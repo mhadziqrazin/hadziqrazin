1a8cf7fcb165edfb5c87a0a82af29345e07752dc
mhadziqrazin <mhadziqrazin@gmail.com>
2024-09-06 14:39:21 +0700

    feat: adjust name size

--- src/lib/sections/About.svelte
+++ src/lib/sections/About.svelte
@@ -10,32 +10,23 @@
 </script>
 
 {#key animate}
-  <section class={`${animate ? 'opacity-100' : 'opacity-0'} height-screen container relative mx-auto px-4 max-w-[700px] md:max-w-[800px] flex flex-col justify-center items-center gap-4 sm:gap-10`}>
-    <h1 in:fly={{y:20, duration: 1000}} class="flex text-2xl sm:text-3xl sm:leading-[3.5rem] text-dark/25 font-extrabold">
+  <section class={`${animate ? 'opacity-100' : 'opacity-0'} height-screen container relative mx-auto px-4 max-w-[700px] md:max-w-[800px] flex flex-col justify-center items-center gap-6 sm:gap-10`}>
+    <h1 in:fly={{y:20, duration: 1000}} class="flex text-xl sm:text-3xl sm:leading-[3.5rem] text-dark/25 font-extrabold">
       Hi!&nbsp;<span>:D</span>
     </h1>
 
-    <div class="flex flex-col gap-1 sm:gap-3">
-      <!-- NAME -->
-      <p in:fade={{duration: 1000, delay: 200}} class="hidden min-[470px]:block text-2xl sm:text-4xl font-medium text-dark/[0.85]">
-        Mu&centerdot;ham&centerdot;mad <span class="font-bold text-dark">Ha&centerdot;dziq</span> Ra&centerdot;zin <span class="text-dark/40">/'h&#593;z&#618;k/</span>
-      </p>
-      <div in:fade={{duration: 1000, delay: 200}} class="min-[470px]:hidden text-2xl font-medium text-dark/[0.85]">
-        <p>
-          Mu&centerdot;ham&centerdot;mad
+    <div class="flex flex-col gap-3">
+      <div>
+        <!-- NAME -->
+        <p in:fade={{duration: 1000, delay: 200}} class="text-lg sm:text-4xl font-medium text-dark/[0.85]">
+          Mu&centerdot;ham&centerdot;mad <span class="font-bold text-dark">Ha&centerdot;dziq</span> Ra&centerdot;zin <span class="text-dark/40">/'h&#593;z&#618;k/</span>
         </p>
-        <p class="font-bold text-dark">
-          Ha&centerdot;dziq <span class="text-dark/40 font-medium">/'h&#593;z&#618;k/</span>
-        </p>
-        <p>
-          Ra&centerdot;zin
+  
+        <!-- OTHER NAMES -->
+        <p in:fly={{y:-5, duration: 1000, delay: 400}} class="text-[0.65rem] sm:text-lg text-dark/60 font-light">
+          Other names: Hajik, Hajiq, Haziq, Hazik, Ojin, wait what.
         </p>
       </div>
-
-      <!-- OTHER NAMES -->
-      <p in:fly={{y:-5, duration: 1000, delay: 400}} class="text-[0.65rem] sm:text-lg text-dark/60 font-light">
-        Other names: Hajik, Hajiq, Haziq, Hazik, Ojin, wait what.
-      </p>
 
       <!-- DESCRIPTION -->
       <div in:fade={{duration: 1000, delay: 200}} class="text-xl/7 sm:text-3xl/10 font-medium text-dark/80 flex flex-col gap-2 sm:gap-4">
