7e35ad4b906940bb21b8bc5a3756dff6b2876c14
mhadziqrazin <mhadziqrazin@gmail.com>
2025-03-19 11:25:39 +0700

    feat: update resume

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,7 @@
       Created by <span class="font-semibold">Muhammad Hadziq Razin</span> &copy; {year}. All Rights Reserved.
     </p>
     <p class="text-[0.4rem] lg:text-sm font-light text-dark/80">
-      Last updated: 16/03/2025
+      Last updated: 19/03/2025
     </p>
   </div>
 </div>
