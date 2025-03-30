926e2545479bcf349ea522715cf1c9369b536ec9
mhadziqrazin <mhadziqrazin@gmail.com>
2025-03-31 00:22:55 +0700

    feat: badge component

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/cn"
+  export let className: string
+</script>
+
+<span class={cn(
+  "h-[31px] w-fit rounded-full px-4 text-sm text-light flex items-center bg-secondary",
+  className
+)}>
+  <slot />
+</span>
